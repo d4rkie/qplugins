321d9b932aad2fecafa4aa8d186e6eeb9290f883
shaohao <shaohao@1b89743e-a014-0410-9af5-f310de1fd27a>
2006-08-08 16:43:26 +0000

    Version 1.5

--- QMPCLIEnc/trunk/QMPCLIEnc.h
+++ QMPCLIEnc/trunk/QMPCLIEnc.h
@@ -2,15 +2,14 @@
 
 #include "QCDEncodeDLL.h"
 
-#include "CLIEncoderPreset.h"
 #include "CLIEncoder.h"
-#include "CLIEncoderUI.h"
 
-
-extern QCLIEncoderPreset	g_cliEP;
-extern QCLIEncoder			g_cliEnc;
-extern ENCODER_PRESET		g_epCur;
-extern BOOL					g_bNoWAVHeader;
-extern BOOL					g_bShowConsole;
-extern TCHAR				g_szEPFile[MAX_PATH];
+extern CString		g_strPath;
+extern CString		g_strParameter;
+extern CString		g_strExtension;
+extern QCLIEncoder	g_cliEnc;
+extern BOOL			g_bDoTag;
+extern BOOL			g_bNoWAVHeader;
+extern BOOL			g_bShowConsole;
+extern TCHAR		g_szEPFile[MAX_PATH];
 
