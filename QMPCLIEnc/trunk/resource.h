59b2d84c81a088c892d614a092dfae8c2328e439
shaohao <shaohao@1b89743e-a014-0410-9af5-f310de1fd27a>
2006-08-08 16:41:52 +0000

    Update UI

--- QMPCLIEnc/trunk/resource.h
+++ QMPCLIEnc/trunk/resource.h
@@ -6,29 +6,28 @@
 #define IDD_SETTINGS                    102
 #define IDD_PRESETS                     103
 #define IDD_ABOUT                       104
-#define IDR_NEW_MENU                    105
 #define IDR_PRESETFILE_MENU             106
+#define IDD_PPP                         107
 #define IDC_TAB                         1000
 #define IDC_EP_TREE                     1001
+#define IDC_URL_VENDOR                  1001
 #define IDC_NEW                         1002
 #define IDC_DELETE                      1003
-#define IDC_PRESETFILE                  1004
+#define IDC_LOAD                        1004
 #define IDC_PAT                         1005
 #define IDC_BP                          1006
 #define IDC_PAR                         1007
 #define IDC_EXT                         1008
 #define IDC_UPDATE                      1009
-#define IDC_BUTTON1                     1020
 #define IDC_SAVE                        1020
 #define IDC_URL_ENCODERS                1020
+#define IDC_DO_TAG                      1021
 #define IDC_PATH                        5001
 #define IDC_BROWSE_PATH                 5002
 #define IDC_PARAMETER                   5003
 #define IDC_EXTENSION                   5004
 #define IDC_NO_WAV_HEADER               5011
 #define IDC_SHOW_CONSOLE                5012
-#define ID_NEW_PRESET                   40014
-#define ID_NEW_CATALOG                  40015
 #define ID_PRESETFILE_IMPORT            40016
 #define ID_PRESETFILE_EXPORT            40017
 
@@ -36,9 +35,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40018
-#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40019
+#define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
