1cd247ddfd4d64d91ab9e2b8298a5b4e996fda2d
shaohao <shaohao@1b89743e-a014-0410-9af5-f310de1fd27a>
2006-08-25 15:52:01 +0000

    Add icons and titles for tooltips.

--- QMPCLIEnc/trunk/SettingsDlg.h
+++ QMPCLIEnc/trunk/SettingsDlg.h
@@ -27,8 +27,6 @@ public:
 		MSG_WM_INITDIALOG(OnInitDialog)
 		COMMAND_ID_HANDLER_EX(IDC_BROWSE_PATH, OnBrowsePath)
 		MESSAGE_HANDLER_EX(WM_PN_DIALOGSAVE, OnDialogSave)
-		//MESSAGE_RANGE_HANDLER_EX(WM_MOUSEFIRST, WM_MOUSELAST, OnMouseMessage)
-		//NOTIFY_CODE_HANDLER_EX(TTN_GETDISPINFO, OnToolTipNotify)
 	END_MSG_MAP()
 
 	// DDX
@@ -47,17 +45,14 @@ public:
 		// DDX controls, Hook it.
 		DoDataExchange( FALSE);
 
-		//// Create the tool tip control
-		//m_ctrlToolTip.Create( m_hWnd, NULL, NULL, TTS_NOPREFIX | TTS_BALLOON);
-		//m_ctrlToolTip.SetMaxTipWidth( 500);
-
-		//// add tools
-		//CToolInfo ti(TTF_SUBCLASS, GetDlgItem( IDC_PARAMETER));
-		//m_ctrlToolTip.AddTool( ti);
-
 		// set property of tool tip control
 		GetTT().SetDelayTime( TTDT_AUTOPOP, 10000); // set the show delay to 10 seconds
 		GetTT().SetMaxTipWidth( 500); // enable multi-line tooltips
+
+		// set icon and title of tool tip control
+		TTSetTitle( IDC_PARAMETER, 1, "REQUIRED");
+		TTSetTitle( IDC_NO_WAV_HEADER, 1, "NOT RECOMMENDED");
+		TTSetTitle( IDC_SHOW_CONSOLE, 1, "OPTIONAL");
 
 		return TRUE;
 	}
@@ -69,28 +64,6 @@ public:
 		if ( fileDlg.DoModal() == IDOK)
 			SetDlgItemText( IDC_PATH, fileDlg.m_szFileName);
 	}
-
-	//LRESULT OnToolTipNotify(LPNMHDR lpNMHDR)
-	//{
-	//	LPNMTTDISPINFO lpTTDI = (LPNMTTDISPINFO)lpNMHDR;
-
-	//	if ( lpTTDI->hdr.idFrom == (UINT_PTR)GetDlgItem( IDC_PARAMETER).m_hWnd) { // Tool tip for parameter edit control
-	//		m_strToolTip.LoadString( IDS_TT_PARAMETER);
-	//		lpTTDI->lpszText = (LPTSTR)(LPCTSTR)m_strToolTip;
-	//	}
-
-	//	return TRUE;
-	//}
-
-	//LRESULT OnMouseMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
-	//{
-	//	MSG msg = { m_hWnd, uMsg, wParam, lParam };
-	//	if ( m_ctrlToolTip.IsWindow())
-	//		m_ctrlToolTip.RelayEvent( &msg);
-	//	
-	//	SetMsgHandled( FALSE);
-	//	return TRUE;
-	//}
 
 	LRESULT OnDialogSave(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
