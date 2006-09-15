99b85087bf668c8e78a6f3bbd785431d0c34dbc9
shaohao <shaohao@1b89743e-a014-0410-9af5-f310de1fd27a>
2006-09-15 11:58:42 +0000

    updated!

--- QMPCLIEnc/trunk/QMPCLIEnc.h
+++ QMPCLIEnc/trunk/QMPCLIEnc.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "QCDEncodeDLL.h"
+#include "IQCDTagInfo.h"
 
 #include "CLIEncoder.h"
 
 #define PLUGIN_NAME "CLI Encoder"
 #define PLUGIN_FULL_NAME "Commandline Encoder"
-#define PLUGIN_VERSION "1.62"
+#define PLUGIN_VERSION "1.7"
 
 extern CString		g_strPath;
 extern CString		g_strParameter;
