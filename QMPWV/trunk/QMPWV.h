0532d7c86b1256eded9fb85f7f07a8a6d22cbef9
shaohao <shaohao@1b89743e-a014-0410-9af5-f310de1fd27a>
2006-11-12 05:33:57 +0000

    Plug-in export implement.

---
+++ QMPWV/trunk/QMPWV.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "resource.h" // resource
+
+extern HINSTANCE	hInstance;    // The unique module instance handle
+
+//////////////////////////////////////////////////////////////////////////
+
+extern INT g_bUseWVC;
+
