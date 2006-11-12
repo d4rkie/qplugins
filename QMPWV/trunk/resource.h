44c6dbc8fc085a68825c2a478d09557ddf1ccfd1
shaohao <shaohao@1b89743e-a014-0410-9af5-f310de1fd27a>
2006-11-12 05:34:25 +0000

    All RG UIs removed.

--- QMPWV/trunk/resource.h
+++ QMPWV/trunk/resource.h
@@ -4,15 +4,11 @@
 //
 #define IDD_CONFIG                      101
 #define IDD_ABOUT                       102
-#define IDS_RG_DISABLE                  103
-#define IDS_RG_TRACK                    104
-#define IDS_RG_ALBUM                    105
-#define IDS_CLIPPING_CLIP               106
-#define IDS_CLIPPING_SOFTLY             107
 #define IDS_STRING108                   108
-#define IDS_CLIPPING_SCALE              108
 #define IDC_URL_QPLUGINS                1001
 #define IDC_URL_WAVPACK                 1002
+#define IDC_CLOSE                       1003
+#define IDC_WAVPACK_VERNUM              1004
 #define IDC_USEWVC                      1018
 #define IDC_REPLAYGAIN_TEXT             1019
 #define IDC_REPLAYGAIN                  1020
@@ -25,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
